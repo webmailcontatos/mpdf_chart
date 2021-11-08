100283d29b02097ae982f7d0231cdcb0a57a64b3
Thiago Guimarães <thiagoguimaraes@intranet>
2021-11-08 09:27:37 -0300

    Remove symfony

---
+++ tests/Charts/PieTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Pie chart test
+ */
+class PieTest extends TestCase
+{
+    protected function getPdfInstance()
+    {
+
+    }
+
+    /**
+     * Pie test sample
+     */
+    public function testPie(): void
+    {
+
+    }
+}
