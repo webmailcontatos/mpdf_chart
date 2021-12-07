1585c443d5e9a6c8696fd63ef13e91e7247a901b
Thiago Guimarães <thiagoguimaraes@intranet>
2021-12-07 13:51:58 -0300

    Acerto

--- tests/Charts/LineArea/LineAreaSvgTest.php
+++ tests/Charts/LineArea/LineAreaSvgTest.php
@@ -157,4 +157,55 @@ class LineAreaSvgTest extends TestCaseChartPdf
         $expected = file_get_contents(__DIR__ . '/../../files/lineAreaSvg03.pdf');
         $this->compararPdf($expected, $result);
     }
+
+    /**
+     * Test sample line chart
+     */
+    public function testLineScaleLinear03(): void
+    {
+        $gradient = $this->getGradientObject();
+        $stops = $gradient->getStops();
+        $gradient->setX1(0);
+        $gradient->setY1(50);
+        $gradient->setX2(100);
+        $gradient->setY2(50);
+        $stops[0]->setStyle('stop-color:rgb(255,255,0);stop-opacity:1');
+        $stops[1]->setStyle('stop-color:rgb(255,59,0);stop-opacity:1');
+
+        $data = [$this->getDataLine06()[0]];
+        $data[0]->setColor([247, 148, 137]);
+        $data[0]->setLineWidth(0.5);
+        $data[0]->showPoint();
+        $points = $data[0]->getPoints();
+        $initY = 25;
+        $axisY = range($initY, 75, 15);
+        foreach ($points as $point) {
+            $point->setColorFill([255, 255, 255]);
+            $point->setColorDraw([247, 148, 137]);
+            $point->setY($initY);
+            $initY += 5;
+        }
+        $axisX = range(0, 9);
+
+        $pdf = $this->getPdfInstance();
+        $scaleX = new ScaleLinear($axisX, 150, 35);
+        $line = new LineAreaSvg($pdf);
+        $line->setGradientSvg($gradient);
+        $line->setX(35);
+        $line->setY(90);
+        $line->setAlpha(0.8);
+        $line->setWidth(150);
+        $line->setHeight(80);
+        $line->setHorizontalGrid(true);
+        $line->setVerticalGrid(true);
+        $line->setAxisX($axisX);
+        $line->setAxisY($axisY);
+        $line->setLines($data);
+        $line->setScaleX($scaleX);
+        $line->setLineWidth(0.1);
+        $line->write();
+        $result = $pdf->Output('lineAreaSvg04.pdf', Destination::STRING_RETURN);
+        $expected = file_get_contents(__DIR__ . '/../../files/lineAreaSvg04.pdf');
+        $this->compararPdf($expected, $result);
+    }
 }
